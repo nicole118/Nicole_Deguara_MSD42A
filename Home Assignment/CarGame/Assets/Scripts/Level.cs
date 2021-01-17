b1378fbfd33375b4c6d1d5147360842074268cca
nicole118 <nicole118@users.noreply.github.com>
2021-01-17 21:08:30 +0100

    level pt1

---
+++ Home Assignment/CarGame/Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level : MonoBehaviour
+{
+    public void LoadStart()
+    {
+        //loads the first scene in the Project
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        //loads the scene with name LaserDefender
+        SceneManager.LoadScene("CarGame");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
